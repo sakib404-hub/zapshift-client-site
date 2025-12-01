d4156830397b4f7e342ed836db7109e54f0767c6
Md Sakib hossen <akibhossainsakib1040@gmail.com>
2025-12-01 19:43:41 +0600

    payment integration

--- src/Pages/DashBoard/Payment/Payment.jsx
+++ src/Pages/DashBoard/Payment/Payment.jsx
@@ -22,13 +22,36 @@ const Payment = () => {
             };
         }
     })
-    console.log(parcel);
+
+    const handlePayment = async () => {
+        const paymentInfo = {
+            cost: parcel.cost,
+            percelId: parcel._id,
+            senderEmail: parcel.senderEmail,
+            percelName: parcel.percelName
+        }
+
+        try {
+            const res = await axiosSecure.post('/create-checkout-session', paymentInfo)
+            window.location.href = res.data.url;
+        } catch (error) {
+            console.log(error.message)
+        }
+    }
+
     if (isLoading) {
         return <Loader></Loader>
     }
     return (
         <div>
-            This is the Payment Options!
+            <p>
+                Please Pay <span> ${parcel.cost}</span> for  : <strong>{parcel.percelName}</strong>
+            </p>
+            <button
+                onClick={handlePayment}
+                className="px-4 bg-primary py-2 rounded-xl font-semibold hover:scale-110 transition transform duration-200 inline-block">
+                Proceed to Pay
+            </button>
         </div>
     );
 };
