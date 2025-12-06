21173e07a7f012d05cf0e269ac211a13ccbd4346
Md Sakib hossen <akibhossainsakib1040@gmail.com>
2025-12-06 15:45:13 +0600

    sending the access token to the backend

--- src/Hooks/useAxios/useAxios.jsx
+++ src/Hooks/useAxios/useAxios.jsx
@@ -1,10 +1,22 @@
 import axios from 'axios';
-import React from 'react';
+import React, { useEffect } from 'react';
+import useAuth from '../useAuth/useAuth';
 
 const axiosSecure = axios.create({
     baseURL: 'http://localhost:5015'
 })
 const useAxios = () => {
+    const { user } = useAuth();
+    //intercept request
+    useEffect(() => {
+        axiosSecure.interceptors.request.use((config) => {
+            if (user?.accessToken) {
+                config.headers.authorization = `Bearer ${user.accessToken}`
+                return config;
+            }
+        })
+    }, [user])
+
     return axiosSecure;
 };
 
