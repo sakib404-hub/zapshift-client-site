8d18eb6bf62fbd121292e886465a1c7c8cca3e72
Md Sakib hossen <akibhossainsakib1040@gmail.com>
2025-12-06 15:24:22 +0600

    fetched the payment information

--- src/Pages/DashBoard/PaymentHistory/PaymentHistory.jsx
+++ src/Pages/DashBoard/PaymentHistory/PaymentHistory.jsx
@@ -1,9 +1,32 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import useAuth from '../../../Hooks/useAuth/useAuth';
+import useAxios from '../../../Hooks/useAxios/useAxios';
+import Loader from '../../../Components/Loader/Loader';
 
 const PaymentHistory = () => {
+    const { user } = useAuth();
+    const axiosSecure = useAxios();
+    const { data: payments = [], isLoading } = useQuery({
+        queryKey: ['payments', user.email],
+        queryFn: async () => {
+            try {
+                const res = await axiosSecure.get(`/payments?email=${user.email}`)
+                return res.data
+            }
+            catch (error) {
+                console.log(error);
+            }
+        }
+    })
+    if (isLoading) {
+        return <Loader></Loader>
+    }
+    console.log([payments]);
     return (
         <div>
             This is the Payment History!
+            <h2 className='text-2xl'>Payment History :  {payments.length}</h2>
         </div>
     );
 };
